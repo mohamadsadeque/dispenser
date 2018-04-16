a53d30c9972adad1083ee3cb4c6fd9394f6b2db3
mohamadsadeque <30416594+mohamadsadeque@users.noreply.github.com>
2018-04-16 17:32:48 -0300

    Add files via upload

---
+++ receptor/receptor.ino
@@ -0,0 +1,49 @@
+#include <RCSwitch.h>
+//Instacia a Biblioteca
+RCSwitch mySwitch = RCSwitch();
+//variavel responsavel em receber os dados do RF
+int value = 0;
+int dado;
+
+void setup() {
+  Serial.begin(9600);
+  // delay para estabilizacao do Sinal
+  delay(500);
+  //Seta como Receptor/ O "0" é para não interromper, deixar continuo
+  //O Pino padrão é o 2
+  mySwitch.enableReceive(0);
+}
+
+void loop() {
+  //Quado estiver sinal disponivel
+  if (mySwitch.available()) {
+    dado =  mySwitch.getReceivedValue();
+    //recebe na variavel value o Status
+    if ( dado == -9999) {
+      value = 0;
+      delay(250);
+      mySwitch.resetAvailable();
+    }
+    else {
+      if ((value + dado) > 0 ) {
+        value = value + dado;
+      }
+      else {
+        value = 0;
+      }
+      Serial.println(value);
+      delay(250);
+      mySwitch.resetAvailable();
+
+    }
+
+  }
+
+  if (value > 0) {
+    value--;
+  }
+  Serial.println(value);
+  delay(250);
+
+}
+//========================================
---
+++ sensor_agua/sensor_agua.ino
@@ -0,0 +1,67 @@
+
+float vazao; //Variável para armazenar o valor em L/min
+float media = 0; //Variável para fazer a média
+int contaPulso; //Variável para a quantidade de pulsos
+int i = 0; //Variável para segundos
+int Min = 00; //Variável para minutos
+int Horas = 0; // Variável para horas
+float Litros = 0; //Variável para Quantidade de agua
+float MiliLitros = 0; //Variavel para Conversão
+  
+  
+  
+  
+void setup()
+{
+ Serial.begin(9600);
+ delay(3000); 
+ pinMode(2, INPUT);
+ attachInterrupt(0, incpulso, RISING); //Configura o pino 2(Interrupção 0) interrupção
+}
+  
+  
+void loop ()
+{
+ contaPulso = 0;//Zera a variável
+ sei(); //Habilita interrupção
+ delay (1000); //Aguarda 1 segundo
+ cli(); //Desabilita interrupção
+  
+ vazao = contaPulso / 5.5; //Converte para L/min
+ media = media + vazao; //Soma a vazão para o calculo da media
+ i++;
+ Serial.print("Vazao");
+ Serial.print(vazao); //Escreve no display o valor da vazão
+ Serial.print(" L/min "); //Escreve L/min
+ Serial.print("Tempo: ");
+ Serial.print(Horas);
+ Serial.print(":");//Escreve :
+ Serial.print(Min);
+ Serial.print(":"); //Escreve :
+ Serial.println(i); //Escreve a contagem i (segundos)
+ MiliLitros = vazao / 60;
+ Litros = Litros + MiliLitros;
+ Serial.print("Litros: ");
+ Serial.print(Litros);
+ 
+  
+ // Neste conjunto de linhas fizemos a média das leituras obtidas a cada 1 minuto
+ if (i == 60)
+ {
+ Min++;  
+ if (Min >= 60)
+ {
+ Horas++;
+ Min = 0;
+ }
+ media = 0; //Zera a variável media para uma nova contagem
+ i = 0; //Zera a variável i para uma nova contagem
+ }
+  
+}
+  
+  
+void incpulso ()
+{
+ contaPulso++; //Incrementa a variável de pulsos
+}
